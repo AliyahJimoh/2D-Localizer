b128e9bfed49cd2d5dc7dcc2e1dfef2c2ebb9100
AliyahJimoh <jimoha1@mcmaster.ca>
2025-03-14 02:03:05 -0400

    Modify Modules

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 # Control Module
 
 from localization import localize
-import input_format
+from input_format import load_input
 from gtsam import Point2, Pose2
 
 try:
-        beacons, fm_map, fm_robot,range_m = input_format.load_input(f"src/user_input" + ".yaml")
+        beacons, fm_map, fm_robot,range_m = load_input(f"src/user_input" + ".yaml")
 except Exception as e:
         print(f"Error loading input file: {e}")
 
