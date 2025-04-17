260c68587b850c0e2adf0cfadb926118e8a961ca
AliyahJimoh <jimoha1@mcmaster.ca>
2025-04-17 10:03:13 -0400

    Add Between Factor to GTSAM wrapper

--- src/gtsam_wrapper.py
+++ src/gtsam_wrapper.py
@@ -30,6 +30,9 @@ def PriorFactorPoint2(key, point, noise_model):
 def RangeFactor2D(key1, key2, measured, noise_model):
     return gtsam.RangeFactor2D(key1, key2, measured, noise_model)
 
+def BetweenFactor(key1,key2,between, noise_model):
+    return gtsam.BetweenFactorPose2(key1, key2, between, noise_model)
+
 
 def noiseModel_Isotropic_Sigma(dim, sigma):
     return gtsam.noiseModel.Isotropic.Sigma(dim, sigma)
